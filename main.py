3d400fe54b5b35a8a2309a37cea42fb512592048
Leonardo <leonardo.negri@helsinki.fi>
2024-10-07 11:41:10 +0300

    move UI files to UI folder

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from PySide6.QtQml import QQmlApplicationEngine
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
-    qml_file = Path(__file__).resolve().parent / "main.qml"
+    qml_file = Path(__file__).resolve().parent / "UI/main.qml"
     engine.load(qml_file)
     if not engine.rootObjects():
         sys.exit(-1)
